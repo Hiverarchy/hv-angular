325b0559fed7b6565a147dc840e3f189fa84e1ac
golfergeek <golfergeet@gmail.com>
2024-09-04 12:14:47 -0500

    still upgrading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import {provideRouter, withViewTransitions} from '@angular/router';
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
