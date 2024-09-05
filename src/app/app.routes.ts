cd462ffbcbcaffb6ebb148402c7ded298450b02c
golfergeek <golfergeet@gmail.com>
2024-09-05 12:17:03 -0500

    added lazy loading and allow for more bandwith

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 import { loggedInGuard } from './services/guards/loggedIn.guard';
 import { LoginComponent } from './components/login/login.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
-import { EditPostComponent } from './components/edit-post/edit-post.component';
-import { NewPostComponent } from './components/new-post/new-post.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'posts/new', component: NewPostComponent, canActivate: [loggedInGuard] },
-  { path: 'posts/:id', component: PostDetailComponent, canActivate: [loggedInGuard] },
-  { path: 'posts/:id/edit', component: EditPostComponent, canActivate: [loggedInGuard] },
+  { path: 'posts/new', 
+    loadComponent: () => import('./components/new-post/new-post.component').then(c => c.NewPostComponent), 
+    canActivate: [loggedInGuard] 
+  },
+  { path: 'posts/:id', 
+    loadComponent: () => import('./components/post-detail/post-detail.component').then(c => c.PostDetailComponent), 
+    canActivate: [loggedInGuard] 
+  },
+  { path: 'posts/:id/edit', 
+    loadComponent: () => import('./components/edit-post/edit-post.component').then(c => c.EditPostComponent), 
+    canActivate: [loggedInGuard] 
+  },
   { path: '**', redirectTo: '/' } // Catch-all route
   ];
